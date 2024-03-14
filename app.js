72964225c50b8dda929f4547ff84d5b3564fae90
Muddi2232 <154959246+Muddi2232@users.noreply.github.com>
2024-03-14 23:04:43 +0500

    14-03-2024

---
+++ app.js
@@ -0,0 +1,19 @@
+var cities= ['Karachi' , 'Islamabad' ,'Lahore', 'Peshawar' ];
+var copied = cities.slice(2)
+alert (copied)
+
+var str= 'Ramdan Mubarak'
+var splited =str.split(" ")
+console.log(splited)
+
+var arr= ['Muddassir', 'Mateen', 'Mubashir', 'Muzzammil']
+var joinArr= arr.join(' ')
+console.log(joinArr)
+
+var students =["anas", "ahmed", "hassan"]
+students.push("ali")
+console.log(students)
+
+students.pop()
+
+students.unshift("saad")
